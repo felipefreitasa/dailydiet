d9d3710a070de4e9442fb8b25e5bcc020e8756be
FELIPE_FREITAS <felipe.freitas@somapay.com.br>
2023-01-06 08:32:59 -0300

    feat: setting styled-components and creating a theme

---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+  COLORS: {
+    RED_DARK: '#BF3B44',
+    RED_MID: '#F3BABD',
+    RED_LIGHT: '#F4E6E7',
+
+    GREEN_DARK: '#639339',
+    GREEN_MID: '#CBE4B4',
+    GREEN_LIGHT: '#E5F0DB',
+
+    GRAY_1: '#1B1D1E',
+    GRAY_2: '#333638',
+    GRAY_3: '#5C6265',
+    GRAY_4: '#B9BBBC',
+    GRAY_5: '#DDDEDF',
+    GRAY_6: '#EFF0F0',
+    GRAY_7: '#FAFAFA',
+
+    WHITE: '#FFFFFF'
+  },
+  FONT_FAMILY: {
+    REGULAR: 'NunitoSans_400Regular',
+    BOLD: 'NunitoSans_700Bold'
+  },
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XXL: 32
+  }
+}
