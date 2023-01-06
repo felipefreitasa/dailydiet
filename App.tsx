8178c87aeb4b78d93f1900c260ab6d5c0c8cfd8c
FELIPE_FREITAS <felipe.freitas@somapay.com.br>
2023-01-06 08:43:52 -0300

    feat: setting expo google fonts and creating a loading component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { ThemeProvider } from 'styled-components/native';
-
 import theme from './src/theme';
 
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
+
+import { Home } from '@screens/Home';
+
+import { Loading } from '@components/Loading';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-        
+      {fontsLoaded ? <Home/> : <Loading/>}
     </ThemeProvider>
   );
 }
-
