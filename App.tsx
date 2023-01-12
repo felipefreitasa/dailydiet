5889e0e037bd6780dd57420f246466ad00e769b1
FELIPE_FREITAS <felipe.freitas@somapay.com.br>
2023-01-12 08:02:41 -0300

    feat: creating StatusBar

--- App.tsx
+++ App.tsx
@@ -1,17 +1,25 @@
-import { ThemeProvider } from 'styled-components/native';
-import theme from './src/theme';
+import { StatusBar } from 'react-native'
 
-import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
+import { ThemeProvider } from 'styled-components/native'
+import theme from './src/theme'
 
-import { Home } from '@screens/Home';
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans'
 
-import { Loading } from '@components/Loading';
+import { Home } from '@screens/Home'
+
+import { Loading } from '@components/Loading'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold })
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle='dark-content'
+        backgroundColor='transparent'
+        translucent
+      />
+
       {fontsLoaded ? <Home/> : <Loading/>}
     </ThemeProvider>
   );
