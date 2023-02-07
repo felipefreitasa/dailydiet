d1ab7654137f75bcdd51f1f35a4567beb0b44f7e
FELIPE_FREITAS <felipe.freitas@somapay.com.br>
2023-02-07 08:09:23 -0300

    feat: creating gradient at bottom of home screen to hide last meals

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -14,7 +14,8 @@ import { Button } from "@components/Button"
 import { Meal } from "@components/Meal"
 import { Loading } from "@components/Loading"
 
-import { AddMealContainer, AddMealLabel, Container, Date, MealsContainer, MealsDay } from "./styles"
+import { AddMealContainer, AddMealLabel, BottomMealsGradient, Container, Date, MealsContainer, MealsDay } from "./styles"
+
 
 export type MealTypeProps = {
   id: string;
@@ -31,7 +32,6 @@ export function Home(){
   const [isLoading, setIsLoading] = useState(false)
 
   const navigation = useNavigation()
-
 
   function calculateMealsPercentageInTheDiet () {
     const totalMeals = allMeals?.length
@@ -75,6 +75,7 @@ export function Home(){
     <Container>
       <HeaderHome />
 
+
       <MealsPercentageInTheDiet 
         percentage={mealsPercentageInTheDiet}
         onPress={() => navigation.navigate('statistics', { mealsPercentageInTheDiet, meals: allMeals })}
@@ -93,25 +94,29 @@ export function Home(){
       </AddMealContainer>
 
      {isLoading ? <Loading/> : (
-        <MealsContainer>
-          {orderedMeals?.map((date, index) => (
-            <MealsDay key={index}>
-              <Date>
-                {removeDateFormat(date[0].date)}
-              </Date>
+        <>
+          <MealsContainer>
+            {orderedMeals?.map((date, index) => (
+              <MealsDay key={index}>
+                <Date>
+                  {removeDateFormat(date[0].date)}
+                </Date>
 
-              {date.map((mealItem) => (
-                <Meal
-                  key={mealItem.id}
-                  hour={mealItem.hour}
-                  mealName={mealItem.name}
-                  isInTheDiet={mealItem.isInTheDiet}
-                  onPress={() => navigation.navigate('mealDetails', { ...mealItem })}
-                />
-              ))}
-            </MealsDay>
-          ))}
-        </MealsContainer>
+                {date.map((mealItem) => (
+                  <Meal
+                    key={mealItem.id}
+                    hour={mealItem.hour}
+                    mealName={mealItem.name}
+                    isInTheDiet={mealItem.isInTheDiet}
+                    onPress={() => navigation.navigate('mealDetails', { ...mealItem })}
+                  />
+                ))}
+              </MealsDay>
+            ))}
+          </MealsContainer>
+
+          <BottomMealsGradient colors={['#FAFAFA90', '#FAFAFAF2']}/>
+        </>
      )}
     </Container>
   )
